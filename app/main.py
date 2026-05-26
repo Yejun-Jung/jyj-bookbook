e91ddc9c776f4e211b6aeff54ef2dba6054b1241
unknown <once6474@naver.com>
2026-05-26 19:29:03 +0900

    mongodb-update-0526

--- app/main.py
+++ app/main.py
@@ -6,21 +6,25 @@ from pathlib import Path
 
 app = FastAPI()
 
-BASE_DIR=Path(__file__).resolve().parent
-templates = Jinja2Templates(directory=BASE_DIR/ "templates")
+BASE_DIR = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse(
-        request,
-        "index.html",
-        {"title":"북북"}
-    )
+    return templates.TemplateResponse(request, "index.html", {"title": "북북"})
+
 
 @app.get("/search", response_class=HTMLResponse)
-async def read_item(request: Request, q:str):
-    return templates.TemplateResponse(
-        request,
-        "index.html",
-        {"keyword":q}
-    )
+async def read_item(request: Request, q: str):
+    return templates.TemplateResponse(request, "index.html", {"keyword": q})
+
+
+@app.on_event("startup")
+async def on_app_start():
+    print("hello server")
+
+
+@app.on_event("shutdown")
+async def on_app_shutdwn():
+    print("goodbye server")
